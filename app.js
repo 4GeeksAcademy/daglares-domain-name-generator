29c43beb34657bc6104bb7831a22b6cb5a835f5e
DAGarciaLopez <dagarcialopez5@gmail.com>
2024-12-13 03:44:43 +0000

    proyecto terminado

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let pronoun = ["the", "our"];
 
 let adj = ["great", "big"];
 
-let noun = ["jogger", "racoon"];
+let noun = ["jogger", "racoon", "planet"];
 
 let ext = [".com", ".net", ".on", ".er"];
 
@@ -10,8 +10,15 @@ for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       for (let m = 0; m < ext.length; m++) {
-        let domain = pronoun[i] + adj[j] + noun[k] + ext[m];
-        console.log(domain);
+        if (noun[k].endsWith(ext[m].slice(1))) {
+          let tamaño = (ext[m].length)-1;
+          console.log(
+            pronoun[i] + adj[j] + noun[k].slice(0, -tamaño) + ext[m]
+          );
+        } else {
+          let domain = pronoun[i] + adj[j] + noun[k] + ext[m];
+          console.log(domain);
+        }
       }
     }
   }
